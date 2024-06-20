437dc14c87b41009a3174eabdfe3e5dda251909b
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-06-20 14:44:31 +0600

    netlify reload issue solving

--- src/components/category/CategoryCard.jsx
+++ src/components/category/CategoryCard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const CategoryCard = ({ item }) => {
   return (
-    <div className="card w-96 bg-green-200 shadow-xl my-8">
+    <div className="card w-96 bg-green-200 shadow-xl my-8 md:ml-0 lg:ml-0 ml-6">
       <div className="card-body">
         <h2 className="card-title">{item.title}</h2>
         <p>{`${item.details.substring(0, 100)}${
